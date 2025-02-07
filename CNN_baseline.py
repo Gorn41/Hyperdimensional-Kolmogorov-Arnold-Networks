ff1ce47d0d457cfd6510d3e3fca3c90fa1717de3
Gorn41 <gorn41@outlook.com>
2025-02-07 06:47:25 -0800

    fixed small error in CNN_baseline.py

--- CNN_baseline.py
+++ CNN_baseline.py
@@ -8,19 +8,24 @@ class CNN(nn.Module):
         self.conv_layer2 = nn.Conv2d(5, 10, 5)
         self.max_pool2 = nn.MaxPool2d(2, 2)
         self.conv_layer3 = nn.Conv2d(10, 12, 3)
+        self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(1452, 3000)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(3000, 200)
 
     def forward_pass(self, x):
-        
+    
         x = self.conv_layer1(x)
         x = self.max_pool1(x)
         x = self.conv_layer2(x)
         x = self.max_pool2(x)
         x = self.conv_layer3(x)
+
+        x = self.flatten(x)
+        
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
 
         return x
+
