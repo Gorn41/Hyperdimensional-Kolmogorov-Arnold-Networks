5d0e82af15f6f75a79783b34f54c68992cb3166e
Sophia Zhang <sophia.p.zhang@gmail.com>
2025-02-07 01:36:30 -0800

    all jpeg compressions and compression ratio calculator

--- JPEGcompression.py
+++ JPEGcompression.py
@@ -1,46 +1,19 @@
 # Compresses all images in a directory into JPEG-2000s at three different compression levels: 1000, 500, and 100.
 import os
 import cv2
+import shutil
 
-# Path to the directory containing the Tiny-Imagenet images
-tiny_imagenet_path_train = './tiny-imagenet/train'
+mid_quality_level = 30
+low_quality_level = 13
 
-# Path to the directories for the three different compression levels
-compression_1000_path_train = './jpeg-compressions/level-1000/train'
-compression_500_path_train = './jpeg-compressions/level-500/train'
-compression_100_path_train = './jpeg-compressions/level-100/train'
-
-# # List of all class directories in the Tiny-Imagenet train dataset
-# classes = os.listdir(tiny_imagenet_path_train)
-
-# # Loop through all class directories
-# for class_name in classes:
-#     # Path to the class directory
-#     class_path = os.path.join(tiny_imagenet_path_train, class_name, 'images')
-    
-#     # List of all images in the class directory
-#     images = os.listdir(class_path)
-    
-#     # Loop through all images in the class directory
-#     for image_name in images:
-#         # Path to the image
-#         image_path = os.path.join(class_path, image_name)
-
-#         # Read the image
-#         image = cv2.imread(image_path)
-
-#         # Compress the image at three different compression levels
-#         cv2.imwrite(os.path.join(compression_1000_path_train, image_name), image, [cv2.IMWRITE_JPEG2000_COMPRESSION_X1000])
-#         cv2.imwrite(os.path.join(compression_500_path_train, image_name), image, [cv2.IMWRITE_JPEG2000_COMPRESSION_X500])
-#         cv2.imwrite(os.path.join(compression_100_path_train, image_name), image, [cv2.IMWRITE_JPEG2000_COMPRESSION_X100])
+# TEST FILES
 
 # Path to the directory containing the Tiny-Imagenet test images
 tiny_imagenet_path_test = './tiny-imagenet/test/images'
 
 # Path to the directories for the three different compression levels
-compression_100_path_test = './jpeg-compressions/level-100/test'
-compression_50_path_test = './jpeg-compressions/level-50/test'
-compression_10_path_test = './jpeg-compressions/level-10/test'
+compression_mid_quality_path_test = './JPEG-compressions/mid_quality/test/images'
+compression_low_quality_path_test = './JPEG-compressions/low_quality/test/images'
 
 # List of all images in the Tiny-Imagenet test dataset
 images = os.listdir(tiny_imagenet_path_test)
@@ -54,10 +27,89 @@ for image_name in images:
     image = cv2.imread(image_path)
 
     # Compress the image at three different compression levels
-    cv2.imwrite(os.path.join(compression_100_path_test, image_name), image, [cv2.IMWRITE_JPEG_QUALITY, 70])
-    cv2.imwrite(os.path.join(compression_50_path_test, image_name), image, [cv2.IMWRITE_JPEG_QUALITY, 30])
-    cv2.imwrite(os.path.join(compression_10_path_test, image_name), image, [cv2.IMWRITE_JPEG_QUALITY, 10])
+    cv2.imwrite(os.path.join(compression_mid_quality_path_test, image_name), image, [cv2.IMWRITE_JPEG_QUALITY, mid_quality_level])
+    cv2.imwrite(os.path.join(compression_low_quality_path_test, image_name), image, [cv2.IMWRITE_JPEG_QUALITY, low_quality_level])
 
-print('All images compressed successfully!')
+print('All test images compressed successfully!')
 
+# VALIDATION FILES
+tiny_imagenet_path_val = './tiny-imagenet/val/images'
 
+compression_mid_quality_path_val = './JPEG-compressions/mid_quality/val/images'
+compression_low_quality_path_val = './JPEG-compressions/low_quality/val/images'
+
+# List of all images in the Tiny-Imagenet validation dataset
+images = os.listdir(tiny_imagenet_path_val)
+
+# Loop through all images in the validation dataset
+for image_name in images:
+    # Path to the image
+    image_path = os.path.join(tiny_imagenet_path_val, image_name)
+
+    # Read the image
+    image = cv2.imread(image_path)
+
+    # Compress the image at three different compression levels
+    cv2.imwrite(os.path.join(compression_mid_quality_path_val, image_name), image, [cv2.IMWRITE_JPEG_QUALITY, mid_quality_level])
+    cv2.imwrite(os.path.join(compression_low_quality_path_val, image_name), image, [cv2.IMWRITE_JPEG_QUALITY, low_quality_level])
+
+print('All validation images compressed successfully!')
+
+# TRAIN FILES
+
+# Path to the directory containing the Tiny-Imagenet images
+tiny_imagenet_path_train = './tiny-imagenet/train'
+
+# Path to the directories for the two compression levels
+compression_mid_quality_path_train = './JPEG-compressions/mid_quality/train'
+compression_low_quality_path_train = './JPEG-compressions/low_quality/train'
+
+# List of all directories in the Tiny-Imagenet training dataset
+directories = os.listdir(tiny_imagenet_path_train)
+
+# Loop through all directories in the training dataset
+for directory in directories:
+
+    # Skip directories that start with a period
+    if directory.startswith('.'):
+        continue
+
+    # Create a new directory for the class
+    if not os.path.exists(os.path.join(compression_mid_quality_path_train, directory)):
+        os.mkdir(os.path.join(compression_mid_quality_path_train, directory))
+    if not os.path.exists(os.path.join(compression_low_quality_path_train, directory)):
+        os.mkdir(os.path.join(compression_low_quality_path_train, directory))
+
+    # Path to the boxes.txt file
+    boxes_path = os.path.join(tiny_imagenet_path_train, directory, directory + '_boxes.txt')
+
+    # Copy the boxes.txt file to the new directory
+    if os.path.exists(boxes_path):
+        shutil.copy(boxes_path, os.path.join(compression_mid_quality_path_train, directory))
+        shutil.copy(boxes_path, os.path.join(compression_low_quality_path_train, directory))
+
+    # Path to the directory containing the images
+    directory_path = os.path.join(tiny_imagenet_path_train, directory, 'images')
+
+    # Create a new directory for the images
+    if not os.path.exists(os.path.join(compression_mid_quality_path_train, directory, 'images')):
+        os.mkdir(os.path.join(compression_mid_quality_path_train, directory, 'images'))
+    if not os.path.exists(os.path.join(compression_low_quality_path_train, directory, 'images')):
+        os.mkdir(os.path.join(compression_low_quality_path_train, directory, 'images'))
+
+    # List of all images in the directory
+    images = os.listdir(directory_path)
+
+    # Loop through all images in the directory
+    for image_name in images:
+        # Path to the image
+        image_path = os.path.join(directory_path, image_name)
+
+        # Read the image
+        image = cv2.imread(image_path)
+
+        # Compress the image at three different compression levels
+        cv2.imwrite(os.path.join(compression_mid_quality_path_train, directory, 'images', image_name), image, [cv2.IMWRITE_JPEG_QUALITY, mid_quality_level])
+        cv2.imwrite(os.path.join(compression_low_quality_path_train, directory, 'images', image_name), image, [cv2.IMWRITE_JPEG_QUALITY, low_quality_level])
+
+print('All training images compressed successfully!')
--- calculate-ratios.py
+++ calculate-ratios.py
@@ -1,51 +1,95 @@
 import os
 
 # Path to the directories for the three different compression levels
-compression_100_path_train = './jpeg-compressions/level-100/test'
-compression_50_path_train = './jpeg-compressions/level-50/test'
-compression_10_path_train = './jpeg-compressions/level-10/test'
+compression_mid_quality_path_train = './JPEG-compressions/mid_quality/train'
+compression_low_quality_path_train = './JPEG-compressions/low_quality/train'
 
-# Path to directory for the original images
-original_images_path = './tiny-imagenet/test/images'
+# Path to the directory containing the Tiny-Imagenet train folders
+tiny_imagenet_path_train = './tiny-imagenet/train'
+
+# List of all directories in the Tiny-Imagenet training dataset
+directories = os.listdir(tiny_imagenet_path_train)
+
+mid_quality_image_ratios = []
+low_quality_image_rations = []
+
+# Loop through all directories in the training dataset
+for directory in directories:
+
+    if directory.startswith('.'):
+        continue
+
+    # Path to the directory containing the images
+    directory_path = os.path.join(tiny_imagenet_path_train, directory, 'images')
+    
+    # List of all images in the directory
+    images = os.listdir(directory_path)
+    
+    # Loop through all images in the directory
+    for image_name in images:
+        # Path to the image
+        image_path = os.path.join(directory_path, image_name)
+        original_image_size = os.path.getsize(image_path)
+
+        # Path to the compressed images
+        mid_quality_image_path = os.path.join(compression_mid_quality_path_train, directory, 'images', image_name)
+        low_quality_image_path = os.path.join(compression_low_quality_path_train, directory, 'images', image_name)
+
+        # Calculate the compression ratio for the mid-quality compression
+        mid_quality_image_size = os.path.getsize(mid_quality_image_path)
+        mid_quality_image_ratios.append(mid_quality_image_size / original_image_size)
+
+        # Calculate the compression ratio for the low-quality compression
+        low_quality_image_size = os.path.getsize(low_quality_image_path)
+        low_quality_image_rations.append(low_quality_image_size / original_image_size)
+
+# Calculate the average compression ratio for the mid-quality compression
+average_mid_quality_image_ratio = sum(mid_quality_image_ratios) / len(mid_quality_image_ratios)
+
+# Calculate the average compression ratio for the low-quality compression
+average_low_quality_image_ratio = sum(low_quality_image_rations) / len(low_quality_image_rations)
+
+print('Average compression ratio for mid-quality compression on train set:', average_mid_quality_image_ratio)
+print('Average compression ratio for low-quality compression on train set:', average_low_quality_image_ratio)
+
+# Do the same for test and validation datasets 
+
+# Path to the directories for the two different compression levels
+compression_mid_quality_path_test = './JPEG-compressions/mid_quality/test/images'
+compression_low_quality_path_test = './JPEG-compressions/low_quality/test/images'
+
+# Path to the directory containing the Tiny-Imagenet test images
+tiny_imagenet_path_test = './tiny-imagenet/test/images'
 
 # List of all images in the Tiny-Imagenet test dataset
-images = os.listdir(compression_100_path_train)
+images = os.listdir(tiny_imagenet_path_test)
 
-compression_ratios_100 = []
-compression_ratios_50 = []
-compression_ratios_10 = []
-# Loop through all images in the test dataset, get AVERAGE compression ratios
+mid_quality_image_ratios = []
+low_quality_image_rations = []
+
+# Loop through all images in the test dataset
 for image_name in images:
-    # Path to the original image
-    original_image_path = os.path.join(original_images_path, image_name)
-    original_image_size = os.path.getsize(original_image_path)
+    # Path to the image
+    image_path = os.path.join(tiny_imagenet_path_test, image_name)
+    original_image_size = os.path.getsize(image_path)
 
     # Path to the compressed images
-    compressed_image_100_path = os.path.join(compression_100_path_train, image_name)
-    compressed_image_50_path = os.path.join(compression_50_path_train, image_name)
-    compressed_image_10_path = os.path.join(compression_10_path_train, image_name)
+    mid_quality_image_path = os.path.join(compression_mid_quality_path_test, image_name)
+    low_quality_image_path = os.path.join(compression_low_quality_path_test, image_name)
 
-    # Get the sizes of the compressed images
-    compressed_image_100_size = os.path.getsize(compressed_image_100_path)
-    compressed_image_50_size = os.path.getsize(compressed_image_50_path)
-    compressed_image_10_size = os.path.getsize(compressed_image_10_path)
+    # Calculate the compression ratio for the mid-quality compression
+    mid_quality_image_size = os.path.getsize(mid_quality_image_path)
+    mid_quality_image_ratios.append(mid_quality_image_size / original_image_size)
 
-    # Calculate the compression ratios
-    compression_ratio_100 = original_image_size / compressed_image_100_size
-    compression_ratio_50 = original_image_size / compressed_image_50_size
-    compression_ratio_10 = original_image_size / compressed_image_10_size
+    # Calculate the compression ratio for the low-quality compression
+    low_quality_image_size = os.path.getsize(low_quality_image_path)
+    low_quality_image_rations.append(low_quality_image_size / original_image_size)
 
-    # Append the compression ratios to the lists
-    compression_ratios_100.append(compression_ratio_100)
-    compression_ratios_50.append(compression_ratio_50)
-    compression_ratios_10.append(compression_ratio_10)
+# Calculate the average compression ratio for the mid-quality compression
+average_mid_quality_image_ratio = sum(mid_quality_image_ratios) / len(mid_quality_image_ratios)
 
-# Calculate the AVERAGE compression ratios
-average_compression_ratio_100 = sum(compression_ratios_100) / len(compression_ratios_100)
-average_compression_ratio_50 = sum(compression_ratios_50) / len(compression_ratios_50)
-average_compression_ratio_10 = sum(compression_ratios_10) / len(compression_ratios_10)
+# Calculate the average compression ratio for the low-quality compression
+average_low_quality_image_ratio = sum(low_quality_image_rations) / len(low_quality_image_rations)
 
-# Print the AVERAGE compression ratios
-print('Average compression ratio at 70%:', average_compression_ratio_100)
-print('Average compression ratio at 30%:', average_compression_ratio_50)
-print('Average compression ratio at 10%:', average_compression_ratio_10)
+print('Average compression ratio for mid-quality compression on test set:', average_mid_quality_image_ratio)
+print('Average compression ratio for low-quality compression on test set:', average_low_quality_image_ratio)
