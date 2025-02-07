ae89fe560b2b403f45c9086d548a1f482b0193ed
Gorn41 <gorn41@outlook.com>
2025-02-07 06:50:00 -0800

    Create CNN_HDC.py

---
+++ CNN_HDC.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn as nn
+from HDC import HDC
+
+class CNN(nn.Module):
+    def __init__(self):
+        self.conv_layer1 = nn.Conv2d(3, 5, 5)
+        self.max_pool1 = nn.MaxPool2d(2, 2)
+        self.conv_layer2 = nn.Conv2d(5, 10, 5)
+        self.max_pool2 = nn.MaxPool2d(2, 2)
+        self.conv_layer3 = nn.Conv2d(10, 12, 3)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(1452, 3000)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(3000, 200)
+
+    def forward_pass(self, x):
+    
+        x = self.conv_layer1(x)
+        x = self.max_pool1(x)
+        x = self.conv_layer2(x)
+        x = self.max_pool2(x)
+        x = self.conv_layer3(x)
+
+        #pseudocode for HDC flatten
+        hdc_flatten = empty tensor
+        for each channel:
+            hdc_flatten.append(HDC.imagehv(channel))
+        x = hdc_flatten
+        
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+
+        return x
+
