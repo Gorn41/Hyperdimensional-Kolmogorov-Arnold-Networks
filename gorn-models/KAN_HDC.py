8c60f6237400d528c3c4fa93a2167b48c655f902
Nattaput (Gorn) Namchittai <gorn41@outlook.com>
2025-03-14 10:19:09 -0700

    more testing

--- gorn-models/KAN_HDC.py
+++ gorn-models/KAN_HDC.py
@@ -52,7 +52,7 @@ class KANFeatureExtractor(nn.Module):
         
 # This class is a combination of the KANFeatureExtractor and LeHDC classes
 class KAN_HDC(nn.Module):
-    def __init__(self, n_dimensions=700, n_classes=10, n_levels=100, grid_size=5):
+    def __init__(self, n_dimensions=800, n_classes=10, n_levels=100, grid_size=5):
         super(KAN_HDC, self).__init__()
         self.feature_network = KANFeatureExtractor(grid_size=grid_size)
         
@@ -65,7 +65,7 @@ class KAN_HDC(nn.Module):
             min_level=-1,
             max_level=1,
             epochs=90,
-            dropout_rate=0.2,
+            dropout_rate=0.3,
             lr=0.0001,
             device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
         )
