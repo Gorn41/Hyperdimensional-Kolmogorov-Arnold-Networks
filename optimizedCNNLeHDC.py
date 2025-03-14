5dcdae9d52824fde4343385863667a60095173a2
Nattaput (Gorn) Namchittai <gorn41@outlook.com>
2025-03-13 17:56:05 -0700

    testing hyperparams

--- optimizedCNNLeHDC.py
+++ optimizedCNNLeHDC.py
@@ -51,7 +51,7 @@ class KANCFeatureExtractor(nn.Module):
 
 
 class KANCLeHDCModel(nn.Module):
-    def __init__(self, n_dimensions=10000, n_classes=10, n_levels=100, grid_size=5):
+    def __init__(self, n_dimensions=20000, n_classes=10, n_levels=100, grid_size=5):
         super(KANCLeHDCModel, self).__init__()
         self.feature_network = KANCFeatureExtractor(grid_size=grid_size)
         
@@ -63,7 +63,7 @@ class KANCLeHDCModel(nn.Module):
             n_levels=n_levels,
             min_level=-1,
             max_level=1,
-            epochs=30,
+            epochs=80,
             dropout_rate=0.3,
             lr=0.01,
             device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
