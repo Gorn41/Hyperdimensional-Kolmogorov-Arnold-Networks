f76433232e36b10c552bd913d70732e623dfba8b
Sophia Zhang <sophia.p.zhang@gmail.com>
2025-03-14 01:16:47 -0700

    architecture

--- soap-models/KAN_baseline.py
+++ soap-models/KAN_baseline.py
@@ -29,7 +29,7 @@ class KAN(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.flatten = nn.Flatten()
 
-        self.fc1 = nn.Linear(8 * 40 * 40, 512)
+        self.fc1 = nn.Linear(43808, 512)
         self.classifier = nn.Linear(512, 10)  # Output 10 classes for Imagenette
 
     def forward(self, x):
