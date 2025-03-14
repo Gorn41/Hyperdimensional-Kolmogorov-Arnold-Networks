d8c472473d150d5d7d3122a2ecce91813c41ea20
Nattaput (Gorn) Namchittai <gorn41@outlook.com>
2025-03-14 09:18:00 -0700

    more testing

--- gorn-models/KAN_HDC.py
+++ gorn-models/KAN_HDC.py
@@ -29,7 +29,7 @@ class KANFeatureExtractor(nn.Module):
             padding=(1,1)
         )
         self.conv2 = KAN_Convolutional_Layer(in_channels=2,
-            out_channels= 1,
+            out_channels= 2,
             kernel_size= (3,3),
             grid_size = grid_size,
             padding=(1,1)
