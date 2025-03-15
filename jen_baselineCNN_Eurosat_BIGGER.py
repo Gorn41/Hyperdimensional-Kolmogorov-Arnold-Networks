bb00bf7a992cdc3729da883f44e1fa887b11cafe
Nattaput (Gorn) Namchittai <gorn41@outlook.com>
2025-03-14 17:31:31 -0700

    more testing

--- jen_baselineCNN_Eurosat_BIGGER.py
+++ jen_baselineCNN_Eurosat_BIGGER.py
@@ -20,7 +20,7 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3)  
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(32, 16, kernel_size=3)
         self.pool = nn.MaxPool2d(2, 2)
         self.flatten = nn.Flatten()
 
